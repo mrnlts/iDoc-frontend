6a3b5e265eae69e0cd72df491f7f85a7cdf6be7e
marina <marinaaltesmagret@gmail.com>
2021-07-13 18:20:19 +0200

    closes #1; Initial commit: test auth modules and connection frontend-backend, display username.

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class Private extends Component {
 	render() {
+		const { username } = this.props.rest.user;
 		return (
 			<div>
-				<h1>Welcome username</h1>
+				<h1>Welcome {username}</h1>
 			</div>
 		);
 	}
