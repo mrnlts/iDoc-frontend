5a0184dc76932dd933c12e7e33fab8e5256672f3
Marina Altés <mrnlts1@gmail.com>
2023-03-11 11:29:17 +0100

    Updates about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React from 'react';
 
 const About = () => {
-  return (
-    <div className="flex flex-col h-screen justify-center items-center text-justify w-full">
-      <div className="text-left">
-        <p className="text-2xl font-bold mb-5">About <span><img src="./logo.png"/></span></p>
-        </div>
-        <p className="w-3/4 text-sm">iDoc is a single-page app (SPA) where health professionals and patients meet, designed using ReactJS, ExpressJS and deployed with Heroku and Netlify. </p>
-        <br />
-        <p className="w-3/4 text-sm">With iDoc, you can access your clinical history or request appointments easily, knowing that your data is being treated with confidentiality.</p>
-      <br />
-      <br />
-        <p>Developed by <span><a href="https://github.com/mrnlts" target="_blank" rel="noreferrer" className="text-blue-500">marinaCodes.</a></span></p>
-    </div>
-  );
-}
+	return (
+		<div className="flex flex-col h-screen justify-center items-center text-justify w-full">
+			<div className="text-left">
+				<p className="text-2xl font-bold mb-5">
+					About{' '}
+					<span>
+						<img src="./logo.png" />
+					</span>
+				</p>
+			</div>
+			<p className="w-3/4 text-sm">
+				iDoc is a single-page app (SPA) where health professionals and patients meet, designed using ReactJS, ExpressJS
+				and deployed with Fly.io and Netlify.{' '}
+			</p>
+			<br />
+			<p className="w-3/4 text-sm">
+				With iDoc, you can access your clinical history or request appointments easily, knowing that your data is being
+				treated with confidentiality.
+			</p>
+			<br />
+			<br />
+			<p>
+				Developed by{' '}
+				<span>
+					<a href="https://github.com/mrnlts" target="_blank" rel="noreferrer" className="text-blue-500">
+						marinaCodes.
+					</a>
+				</span>
+			</p>
+		</div>
+	);
+};
 
 export default About;
